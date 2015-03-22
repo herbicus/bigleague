68c09a01c30b4f5c2e04b62d89ff0a319301a730
herbicus <sonofman_x@hotmail.com>
2015-03-22 13:12:02 -0400

    animation tinker

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@ var bigleagueAPI = (function(options) {
 	var missionAnimation = new TimelineMax({paused: true});
 	
 	missionAnimation.to(".l-hero-logo, .l-mobile-logo", 0.75, { autoAlpha: 0, zIndex: 1, left: "40%", ease: Back.easeInOut});
-	missionAnimation.to(".l-baseball-img", 0.5, { autoAlpha: 0, zIndex: 1, scale: 0, ease: Quad.easeInOut}, 0.075);
+	missionAnimation.to(".l-baseball-img", 0.5, { autoAlpha: 0, zIndex: 1, scale: 0.8, ease: Quad.easeInOut}, 0.075);
 	missionAnimation.to(".l-bubble-container", 0.5, { autoAlpha: 0, zIndex: 1, right: "3%", ease: Back.easeInOut}, 0.075);
 	missionAnimation.to(".l-hero-mission, .l-mobile-mission", 0.75, { autoAlpha: 1, ease: Back.easeInOut}, 0.025);
 
