b1667e62732969f35bb8df26722b57e3181d4ef5
herbicus <sonofman_x@hotmail.com>
2015-03-22 12:58:37 -0400

    fixed animations and header stylings

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,24 +7,25 @@ var bigleagueAPI = (function(options) {
 	var shared = {},
 		options = options || {}
 	
-		var missionAnimation = new TimelineMax({paused: true});
-		missionAnimation.to(".l-hero-logo, .l-mobile-logo", 0.75, { autoAlpha: 0, zIndex: 1, left: "40%", ease: Back.easeInOut});
-		missionAnimation.to(".l-baseball-img", 0.5, { autoAlpha: 0, zIndex: 1, scale: 0, ease: Quad.easeInOut}, 0.075);
-		missionAnimation.to(".l-bubble-container", 0.5, { autoAlpha: 0, zIndex: 1, right: "3%", ease: Back.easeInOut}, 0.075);
-		missionAnimation.to(".l-hero-mission, .l-logo-mission", 0.75, { autoAlpha: 1, ease: Back.easeInOut}, 0.075);
+	var missionAnimation = new TimelineMax({paused: true});
+	
+	missionAnimation.to(".l-hero-logo, .l-mobile-logo", 0.75, { autoAlpha: 0, zIndex: 1, left: "40%", ease: Back.easeInOut});
+	missionAnimation.to(".l-baseball-img", 0.5, { autoAlpha: 0, zIndex: 1, scale: 0, ease: Quad.easeInOut}, 0.075);
+	missionAnimation.to(".l-bubble-container", 0.5, { autoAlpha: 0, zIndex: 1, right: "3%", ease: Back.easeInOut}, 0.075);
+	missionAnimation.to(".l-hero-mission, .l-mobile-mission", 0.75, { autoAlpha: 1, ease: Back.easeInOut}, 0.025);
 
-		
-		// Mission Toggle Btn
-		$(".l-mission-btn").on("click", function(){
-			$(this).toggleClass("open");
+	
+	// Mission Toggle Btn
+	$(".l-mission-btn").on("click", function(){
+		$(this).toggleClass("open");
 
-	  		if ($(this).hasClass("played")) {
-	  			missionAnimation.reverse();	
-	  		} else {
-	  			missionAnimation.play();
-	  		}	
-	  		$(this).toggleClass("played");
-		});
+  		if ($(this).hasClass("played")) {
+  			missionAnimation.reverse();	
+  		} else {
+  			missionAnimation.play();
+  		}	
+  		$(this).toggleClass("played");
+	});
 	
 
 	// init
@@ -39,5 +40,4 @@ var bigleagueAPI = (function(options) {
 
 $(document).ready(function() {
 	bigleagueAPI.init();
-
 });
