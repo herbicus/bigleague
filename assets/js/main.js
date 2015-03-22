9d78ed762ec107637eeef2b8124752a9eefe8af5
herbicus <sonofman_x@hotmail.com>
2015-03-21 23:40:41 -0400

    mission animation added

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,21 @@ var bigleagueAPI = (function(options) {
 	var shared = {},
 		options = options || {}
 	
+		var missionAnimation = new TimelineMax({paused: true});
+		missionAnimation.to(".l-hero-logo, .l-mobile-logo", 0.5, { autoAlpha: 0, zIndex: 1, left: "40%", ease: Back.easeInOut});
+		missionAnimation.to(".l-hero-mission, .l-logo-mission", 0.5, { autoAlpha: 1, ease: Back.easeInOut}, 0.25);
 
+		// MOBILE ICON - HAMBURGER
+		$(".l-mission-btn").on("click", function(){
+			$(this).toggleClass("open");
+
+	  		if ($(this).hasClass("played")) {
+	  			missionAnimation.reverse();	
+	  		} else {
+	  			missionAnimation.play();
+	  		}	
+	  		$(this).toggleClass("played");
+		});
 	
 
 	// init
